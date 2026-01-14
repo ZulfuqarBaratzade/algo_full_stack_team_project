bdfc046ed6566aff1978f95c9ba952565df68bf3
mehracceferov <mehraccfrov340@gmail.com>
2026-01-14 12:33:40 +0400

    asaide section

--- market.com/src/components/Header.jsx
+++ market.com/src/components/Header.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import '../styles/Header.css'
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <div className='Navbar'>
+        <div className='logo'>
+          <h1><span className=''>Bazarstore.Az</span></h1>
+        </div>
+        <select>
+          <option>Catdirilma Unvan secin</option>
+        </select>
+        <input placeholder='Mehsul axtar' /><i class="fa-solid fa-magnifying-glass"></i>
+        <div className='const'>
+          <i class="fa-regular fa-user"></i>
+          <i class="fa-regular fa-heart"></i>
+          <i class="fa-solid fa-cart-shopping"></i>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
