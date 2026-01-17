66da1f968497501fa92b19c17a2a8eef06decd77
Ilkin Mahmudov <ilkinmahmudov428@example.com>
2026-01-17 13:18:16 +0400

    Ready

--- market.com/src/pages/Home.jsx
+++ market.com/src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Aside from '../components/Aside'
+
 import Header from '../components/Header'
+
+
+
 
 function Home() {
   return (
     <div>
-      <Header />
-      <Aside/>
+      <Header/>
+      <Aside />
+
+
     </div>
   )
 }
