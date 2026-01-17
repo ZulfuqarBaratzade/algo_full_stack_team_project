235071e6a3ea5d20742d5d88974dcd8e669dccfe
Ilkin Mahmudov <ilkinmahmudov428@example.com>
2026-01-17 12:42:19 +0400

    ready

--- market.com/src/pages/Home.jsx
+++ market.com/src/pages/Home.jsx
@@ -1,18 +1,11 @@
 import React from 'react'
-<<<<<<< HEAD
 import Header from '../components/Header'
+import Aside from '../components/Aside'
 function Home() {
   return (
     <div>
       <Header/>
-=======
-import Aside from '../components/Aside'
-
-function Home() {
-  return (
-    <div>
-      <Aside/>
->>>>>>> 914165c542e39c06c04514dc16f133966d6d5b42
+      <Aside />
     </div>
   )
 }
