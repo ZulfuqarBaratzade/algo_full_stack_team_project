f9ad809fbe3c3236ad7f5f74f7d7764512a0d50a
mehracceferov <mehraccfrov340@gmail.com>
2026-01-17 13:12:54 +0400

    nav

--- market.com/src/pages/Home.jsx
+++ market.com/src/pages/Home.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
-import Header from '../components/Header'
 import Aside from '../components/Aside'
-<<<<<<< HEAD
+import Header from '../components/Header'
 
 function Home() {
   return (
     <div>
       <Header />
       <Aside/>
-=======
-function Home() {
-  return (
-    <div>
-      <Header/>
-      <Aside />
->>>>>>> 83f1e7218ff225a2f1101725372b78b775ba4187
     </div>
   )
 }
