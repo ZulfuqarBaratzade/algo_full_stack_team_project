27128187cf0938b6a7110a8df9fb01d5b0bb1763
mehracceferov <mehraccfrov340@gmail.com>
2026-01-17 13:07:45 +0400

    hazir

--- market.com/src/components/Aside.jsx
+++ market.com/src/components/Aside.jsx
@@ -6,11 +6,11 @@ function Aside() {
         <div className="aside-sec">
             <ul>
                 <li className='allCategory'><a href="#" ><i class="fa-solid fa-bars"></i> Butun Kategoriyalar</a></li>
-                <li><a href="#"><i class="fa-solid fa-arrow-right"></i>Meyve, Terevez</a></li>
-                <li><a href="#"><i class="fa-solid fa-arrow-right"></i>Et-Toyuq, Baliq</a></li>
-                <li><a href="#"><i class="fa-solid fa-arrow-right"></i>Sud, Seher Yemeyi</a></li>
-                <li><a href="#"><i class="fa-solid fa-arrow-right"></i>Tutun Mehsullari </a></li>
-                <li><a href="#"><i class="fa-solid fa-arrow-right"></i>Usaq Dunyasi</a></li>
+                <li><a href="#"><i class="fa-solid fa-arrow-right"></i>🍉Meyve, Terevez<i class="fa-solid fa-angle-right"></i></a></li>
+                <li><a href="#"><i class="fa-solid fa-arrow-right"></i>🍗Et-Toyuq, Baliq<i class="fa-solid fa-angle-right"></i></a></li>
+                <li><a href="#"><i class="fa-solid fa-arrow-right"></i>🥛Sud, Seher Yemeyi<i class="fa-solid fa-angle-right"></i></a></li>
+                <li><a href="#"><i class="fa-solid fa-arrow-right"></i>🚬Tutun Mehsullari<i class="fa-solid fa-angle-right"></i> </a></li>
+                <li><a href="#"><i class="fa-solid fa-arrow-right"></i>👶Usaq Dunyasi<i class="fa-solid fa-angle-right"></i></a></li>
             </ul>
         </div>
     </div>
--- market.com/src/pages/Home.jsx
+++ market.com/src/pages/Home.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
-<<<<<<< HEAD
 import Header from '../components/Header'
-function Home() {
-  return (
-    <div>
-      <Header/>
-=======
 import Aside from '../components/Aside'
 
 function Home() {
   return (
     <div>
+      <Header />
       <Aside/>
->>>>>>> 914165c542e39c06c04514dc16f133966d6d5b42
     </div>
   )
 }
